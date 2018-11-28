a8ff1206081c878482386d34ad7c4596b75e5f74
kosuke.onaru <onaru.kosuke@imagicalab.co.jp>
2018-11-28 07:43:19 +0000

    retake

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,5 +47,9 @@ class TasksController < ApplicationController
     flash[:success] = 'タスクが削除されました'
     redirect_to tasks_path
   end
-  
 end
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
