aaf0ff06a8a6bf12a7e06a5e060c935ae6965e79
kosuke.onaru <onaru.kosuke@imagicalab.co.jp>
2018-11-29 09:15:50 +0000

    add colum retake

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
-  #content,titleが空っぽでないか。255字を超えてないか
   validates :content, presence: true, length: { maximum: 255 }
   validates :status, presence: true, length: { maximum: 10 }
 end
