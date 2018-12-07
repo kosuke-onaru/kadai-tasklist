363c95d9946a0201f33b410c90528b0335bea2ef
kosuke.onaru <onaru.kosuke@imagicalab.co.jp>
2018-12-07 02:25:50 +0000

    login task#index=>root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root to: 'tasks#index'
-  #resources :tasks
+  root to: 'tasks#index'
+  resources :tasks
   
-  root to: 'toppages#index'
+  #root to: 'toppages#index'
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  resources :toppages, only: [:index]
+  
 end
