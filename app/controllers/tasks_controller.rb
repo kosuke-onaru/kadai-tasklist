40486ef95719ed22d496573944f6872b5f03f9f3
kosuke.onaru <onaru.kosuke@imagicalab.co.jp>
2018-12-07 10:07:42 +0000

    lesson12

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
 class TasksController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:index]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all.page(params[:page])
+    #@tasks = Task.all.page(params[:page])
+    if logged_in?
+      # form_for 用
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end
   end
 
   def show
@@ -14,12 +20,13 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
-
+    #@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash[:danger] = 'タスクが投稿されません'
       render :new
     end
@@ -58,4 +65,14 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @micropost
+      redirect_to toppages_path
+    end
+  end
+  
+  
+  
 end
